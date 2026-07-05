d8ffe95c6c3f014620352ef100e5b33a582d7a9b
itzmitto <andre.babirian2004@gmail.com>
2026-07-05 20:49:25 +0200

    styling aangepast van de hero image

--- resources/views/sections/hero.blade.php
+++ resources/views/sections/hero.blade.php
@@ -1,18 +1,15 @@
-<section class="max-w-[2000px] mx-auto px-6 mt-6">
+<section class="max-w-[1500px] mx-auto px-6 mt-6">
 
-    <div class="relative overflow-hidden rounded-3xl">
+    <div class="relative rounded-3xl overflow-hidden">
 
-       <img
-    src="{{ asset('images/image1.png') }}"
-    alt="Coffee"
-    class="w-full h-[520px] object-cover rounded-3xl">
+        <img
+            src="{{ asset('images/image1.png') }}"
+            alt="Coffee"
+            class="w-full aspect-[2.25/1] object-cover">
 
-        <div class="absolute bottom-10 left-10 text-white">
-
+        <div class="absolute bottom-8 left-8 text-white">
             <h1 class="text-5xl font-bold leading-tight">
-                Bringing Art to<br>everything
-            </h1>
-
+                Bringing Art to<br>everything</h1>
         </div>
 
     </div>
